a0fbbb9064fbd17fd36bcb974943c62a46d79d41
kitkatyourmom <sarahlridings@gmail.com>
2023-05-24 21:02:04 -0600

    add DOM variables to script js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// main page variables
+const mainContainer = $('.main-container');
+const mainCityZipInput = $('#city-zip-input');
+const mainSearchBtn = $('#main-search-btn');
+
+let cityZipInput;
+
+// search results page variables
+const searchedArea = $('#searchedArea');
+const searchResultsContainer = $('#searchResults');
+
+// brewery info page variables
+const breweryContainer = $('#brewery-container');
+const breweryName = $('#brewery-name');
+const breweryLogo = $('#brewery-logo');
+const breweryAboutContainer = $('#about-container');
+
+// directions page variables 
+const navSidebar = $('#sidebar');
+const navUserLocation = $('#userlocation');
+const navBreweryAddress = $('#breweryaddress');
+const navDirectionsSection = $('#directionssection');
+const navDirectionsListContainer = $('#directions-list-container');
+const navMapContainer = $('#maparea');
