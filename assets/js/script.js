fed4c5318f6751ada2edb7f7e127936156df269e
kitkatyourmom <sarahlridings@gmail.com>
2023-05-25 21:24:47 -0600

    add alert to main page and functionality in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,12 @@ const mainContainer = $('.main-container');
 const mainCityZipInput = $('#city-zip-input');
 const mainSearchBtn = $('#main-search-btn');
 
-let cityZipInput;
+// const alertList = $('.alert');
+// const alerts = [...alertList].map(element => new bootstrap.Alert(element));
+
+let locationInput;
+let city;
+let postalCode;
 
 // search results page variables
 const searchedArea = $('#searchedArea');
@@ -23,6 +28,34 @@ const navDirectionsSection = $('#directionssection');
 const navDirectionsListContainer = $('#directions-list-container');
 const navMapContainer = $('#maparea');
 
+
 // coding for main page functionality
 
+$("#main-search-btn").click(function(event){
+    event.preventDefault();
+    // remove error alert if it wasn't dismissed
+    $("#error-alert").addClass("hide");
+    // grab input information
+    // double check if it is a postal code or a city
+    if (city || postalCode) {
+        // use stored information in the variable locationInput
+        window.location.assign("searchresult.html");
+    } else {
+        $("#error-alert").removeClass("hide");
+    };
+});
+
+$("#error-alert-close").click(function(event){
+    event.preventDefault();
+    // error alert close
+    $("#error-alert").addClass("hide");
+    $("#city-zip-input").val('');
+});
+
 // coding for brewery about page
+
+$("#about-directions-btn").click(function(event){
+    event.preventDefault();
+    // pull information from url to put into directions?
+    window.location.assign("directions.html"); 
+});
