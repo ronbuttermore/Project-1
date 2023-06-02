bee7be8b93e49123ec749c0903c0cdacd7bbee3d
kitkatyourmom <sarahlridings@gmail.com>
2023-06-01 19:16:32 -0600

    modify code in index html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
-// main page variables
-const mainContainer = $('.main-container');
-const mainCityZipInput = $('#city-zip-input');
-const mainSearchBtn = $('#main-search-btn');
+// // main page variables
+// const mainContainer = $('.main-container');
+// const mainCityZipInput = $('#city-zip-input');
+// const mainSearchBtn = $('#main-search-btn');
 
-let locationInput;
+// let locationInput;
 let city;
 let postalCode;
 
-// search results page variables
-const searchedArea = $('#searchedArea');
-const searchResultsContainer = $('#searchResults');
+// // search results page variables
+// const searchedArea = $('#searchedArea');
+// const searchResultsContainer = $('#searchResults');
 
-// brewery info page variables
-const breweryContainer = $('#brewery-container');
-const breweryName = $('#brewery-name');
-const breweryLogo = $('#brewery-logo');
-const breweryAboutContainer = $('#about-container');
+// // brewery info page variables
+// const breweryContainer = $('#brewery-container');
+// const breweryName = $('#brewery-name');
+// const breweryLogo = $('#brewery-logo');
+// const breweryAboutContainer = $('#about-container');
 
-// directions page variables 
-const navSidebar = $('#sidebar');
-const navUserLocation = $('#userlocation');
-const navBreweryAddress = $('#breweryaddress');
-const navDirectionsSection = $('#directionssection');
-const navDirectionsListContainer = $('#directions-list-container');
-const navMapContainer = $('#maparea');
+// // directions page variables 
+// const navSidebar = $('#sidebar');
+// const navUserLocation = $('#userlocation');
+// const navBreweryAddress = $('#breweryaddress');
+// const navDirectionsSection = $('#directionssection');
+// const navDirectionsListContainer = $('#directions-list-container');
+// const navMapContainer = $('#maparea');
 
 
 // coding for main page functionality
@@ -37,9 +37,11 @@ $("#main-search-btn").click(function(event){
     if (city || postalCode) {
         // use stored information in the variable locationInput
         window.location.assign("searchresult.html");
+    } else if () { //if there are no results
+        $("#sorry-alert").removeClass("hide");
     } else {
-        $("#error-alert").removeClass("hide");
-    };
+        $("#whoops-alert").removeClass("hide");
+    }
 });
 
 $("#error-alert-close").click(function(event){
@@ -60,6 +62,8 @@ $("#about-directions-btn").click(function(event){
     // pull information from url to put into directions?
     window.location.assign("directions.html"); 
 });
+
+// coding for directions page
 
 const map = tt.map({
     key: "9GgFvkDZz2WjiY63GGreVAvcuKo7Ztvl",
